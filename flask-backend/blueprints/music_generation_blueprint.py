1d2bbcfc9596d6de8c58d6057c7186d7f215a45a
To Hoi Lam <alextohoilam121@gmail.com>
2023-03-23 05:39:43 +0800

    Midway of setup backend for generate music with emotion

--- flask-backend/blueprints/music_generation_blueprint.py
+++ flask-backend/blueprints/music_generation_blueprint.py
@@ -1,10 +1,14 @@
 import os
-from flask import Blueprint
+import random
+import numpy as np
+from flask import request
+from flask import Blueprint, send_file
 from flask_cors import cross_origin
-from blueprints.speech_emotion_recognition_blueprint import getModelConfig
+from blueprints.speech_emotion_recognition_blueprint import getModelConfig, SER_Predict_Full
 from components.music_generation.generate_music import generateMusic
 import magenta.music as mm
 from pretty_midi import PrettyMIDI
+from scipy.io.wavfile import write
 
 
 
@@ -13,16 +17,42 @@ music_generation_blueprint = Blueprint('music_generation', __name__)
 PATH_DIR_NAME = '/music-generation'
 MODEL_PATH = os.path.join("components", "music_generation", "models", "happiness_baseline_attention_model_1.mag")
 PRIMERS_MIDI_PATHS = os.path.join("components", "music_generation", "primers")
+WAV_SAVE_PATH = os.path.join("components", "music_generation", "generated_wav")
+
+SAMPLING_RATE = 16000
+PRIMER_COUNT = 30
 
 @music_generation_blueprint.errorhandler(413)
 def too_large(e):
     return "File is too large", 413
 
-@music_generation_blueprint.route(PATH_DIR_NAME + '/generate')
+@music_generation_blueprint.route(PATH_DIR_NAME + '/generate', methods=['POST'])
 @cross_origin()
 def generate():
-  # primer_path = PRIMERS_MIDI_PATHS[3]
-  primer_path = os.path.abspath(os.path.join(PRIMERS_MIDI_PATHS, "Happiness", "TRAAEEH128E0795DFE_0_primer_10.mid"))
+  result = SER_Predict_Full(request)
+  speech_emotion = result.data[0]
+  emotion = speech_emotion['emotion']
+
+  primer_folder = os.path.abspath(os.path.join(PRIMERS_MIDI_PATHS, emotion))
+  primer_pos = random.randint(0, PRIMER_COUNT - 1)
+  primer_filename = ""
+
+  count = 0
+  for dirname, _, filenames in os.walk(primer_folder):
+    for filename in filenames:
+      if filename[-4:] != ".mid":
+        continue
+
+      if (count == primer_pos):
+        primer_filename = filename
+        break
+
+      count += 1
+    
+    if (primer_filename != ""):
+      break
+
+  primer_path = os.path.join(primer_folder, primer_filename)
 
 
   attention_sequence = generateMusic(
@@ -35,8 +65,11 @@ def generate():
   )
 
   attention_pretty_midi = mm.midi_io.note_sequence_to_pretty_midi(attention_sequence)
-  waveform = attention_pretty_midi.fluidsynth()
-  return {'data': str(waveform)}
+  waveform = attention_pretty_midi.fluidsynth(fs=SAMPLING_RATE)
+  scaled = np.int16(waveform / np.max(np.abs(waveform)) * 32767)
+  filepath = os.path.join(WAV_SAVE_PATH, 'test.wav')
+  write(filepath, SAMPLING_RATE, scaled)
+  return send_file(filepath)
 
 
 
--- flask-backend/blueprints/speech_emotion_recognition_blueprint.py
+++ flask-backend/blueprints/speech_emotion_recognition_blueprint.py
@@ -44,6 +44,59 @@ def models():
 @speech_emotion_recognition_blueprint.route(PATH_DIR_NAME + '/predict', methods=['POST'])
 @cross_origin()
 def predict():
+  return SER_Predict(request)
+
+
+def SER_Predict_Full(api_request):
+  result = SER_Predict(api_request)
+  separated_predicted_list = result['data']
+  packed_predicted_list = []
+
+  currentName = 'null'
+  count = 0
+  temp_packed = None
+  for separated_predicted in separated_predicted_list:
+    if (separated_predicted['name'] != currentName):
+
+      if (temp_packed != None):
+        temp_packed['count'] = count
+        packed_predicted_list.append(temp_packed)
+
+
+      currentName = separated_predicted['name']
+      count = 1
+
+      temp_packed = {
+        'name': separated_predicted['name'],
+        'section': 'All',
+        'emotion': None,
+        'percentage': separated_predicted['percentage'],
+        'count': 1
+      }
+    else:
+      for emotion_label in separated_predicted['percentage']:
+        temp_packed['percentage'][emotion_label] += separated_predicted['percentage'][emotion_label]
+      count += 1
+  
+  if (temp_packed != None):
+    temp_packed['count'] = count
+    packed_predicted_list.append(temp_packed)
+  
+  for packed_predicted in packed_predicted_list:
+    max_emotion = ''
+    max_value = -1
+    for emotion_label in packed_predicted['percentage']:
+      packed_predicted['percentage'][emotion_label] /= packed_predicted['count']
+
+      if (packed_predicted['percentage'][emotion_label] > max_value):
+        max_value = packed_predicted['percentage'][emotion_label]
+        max_emotion = emotion_label
+    
+    packed_predicted['emotion'] = max_emotion
+  
+  return {'data': packed_predicted_list, 'status': 'ok', 'errMsg': ''}
+
+def SER_Predict(api_request):
   print('Speech Emotion Recognition Predict')
   # 1). Get model config
   modelListConfig = getModelConfig()
@@ -54,7 +107,7 @@ def predict():
 
     
   # 2). Check if model choice parameter is passed correctly
-  if ('modelChoice' not in request.form or request.form['modelChoice'] == 'null'):
+  if ('modelChoice' not in api_request.form or api_request.form['modelChoice'] == 'null'):
     errMsg = 'Model is not selected! Please select a model from dropdown!'
     print('Failed: ' + errMsg)
     return {'data': [], 'status': 'failed', 'errMsg': errMsg}
@@ -63,9 +116,9 @@ def predict():
   fileList = []
   filenameList = []
 
-  if (len(request.files) != 0):
-    for filename in request.files:
-      file = request.files[filename]
+  if (len(api_request.files) != 0):
+    for filename in api_request.files:
+      file = api_request.files[filename]
       fileList.append(file)
       filenameList.append(filename)
   else:
@@ -74,7 +127,7 @@ def predict():
     return {'data': [], 'status': 'warning', 'errMsg': warnMsg}
 
   # 4). A: Get Model Choice and Configure Model; B: Load and Process data
-  modelChoice = int(request.form['modelChoice'])
+  modelChoice = int(api_request.form['modelChoice'])
   if (modelListConfig != None):
     status, res = getModelAndData(modelChoice, modelListConfig, fileList, filenameList)
     if (status != 'ok'):
